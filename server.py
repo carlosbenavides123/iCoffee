15bed060946d96472886ef271de6e913ed0b15a2
unknown <carlos7legend@gmail.com>
2020-04-24 15:15:48 -0700

    urgent no direct ssh updates

--- server.py
+++ server.py
@@ -113,12 +113,15 @@ class SimpleEcho(WebSocket):
         return lines
 
     def read_temp(self):
+        print("read temp")
         lines = read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
             lines = read_temp_raw()
+        print("read lines")
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
+            print("equal_pos")
             temp_string = lines[1][equals_pos+2:]
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
@@ -130,12 +133,11 @@ class SimpleEcho(WebSocket):
         temp = 195
         if coffee_type != "Regular Coffee":
             temp = 205
-        print(2)
+        print(2, temp, coffee_type)
         while True:
             print(3)
             c, f = self.read_temp()
-            
-            print(c, f)
+            print(4, c, f)
             if f >= temp:
                 break
 
